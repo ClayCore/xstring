ae06f1072bcd6cd01f7e68d53dff8fc31ab6751d
lunarium <claymorealt@op.pl>
2023-09-24 20:56:56 +0200

    Add xstring type forward declarations

---
+++ src/xstring_defs.h
@@ -0,0 +1,10 @@
+#ifndef XSTRING_DEFS_GUARD_
+#define XSTRING_DEFS_GUARD_
+
+struct xstring_s;
+typedef struct xstring_s xstring;
+
+struct xstring_list_s;
+typedef struct xstring_list_s xstring_list;
+
+#endif /* XSTRING_DEFS_GUARD_ */
