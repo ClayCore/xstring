371602bb49b9ec8d59ec4e133b672858b7e7d681
lunarium <claymorealt@op.pl>
2023-09-25 16:14:08 +0200

    Add `xstring` and `xstring_list` defs

--- src/xstring_defs.h
+++ src/xstring_defs.h
@@ -1,10 +1,24 @@
 #ifndef XSTRING_DEFS_GUARD_
 #define XSTRING_DEFS_GUARD_
 
+#include "util/aliases.h"
+
 struct xstring_s;
 typedef struct xstring_s xstring;
 
 struct xstring_list_s;
 typedef struct xstring_list_s xstring_list;
+
+struct xstring_s
+{
+        char *ptr;
+        usize len;
+};
+
+struct xstring_list_s
+{
+        xstring *ptr;
+        usize len;
+};
 
 #endif /* XSTRING_DEFS_GUARD_ */
